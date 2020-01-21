fee2eaaeac72be45b0ba9597a08d1aaea2643cc0
Tomas-Stevens <tomas.stevens1996@gmail.com>
2020-01-21 11:43:48 -0600

    added shell of bin search/find record

--- database hw 1/src/Main.java
+++ database hw 1/src/Main.java
@@ -19,11 +19,58 @@ public class Main {
     static String FILENAME = "input.txt";
     static String NL = System.lineSeparator();
 
+
+
+
+
+    
     // add binary search here for record finding.
     //we pick the key
 
-
-
+//    /*Get record number n-th (from 1 to 4360) */
+//    //public static String getRecord(RandomAccessFile Din, int recordNum) throws IOException
+//    public static String getRecord(RandomAccessFile Din, int recordNum) throws IOException
+//    {
+//        String record = "NOT_FOUND";
+//        if ((recordNum >=1) && (recordNum <= NUM_RECORDS))
+//        {
+//            Din.seek(0); // return to the top fo the file
+//            Din.skipBytes(recordNum * RECORD_SIZE);
+//            record = Din.readLine();
+//        }
+//        return record;
+//    }
+//
+//    /*Binary Search record id */
+//    public static String binarySearch(RandomAccessFile Din, String id) throws IOException
+//    {
+//        int Low = 0;
+//        int High = NUM_RECORDS-1;
+//        int Middle;
+//        String MiddleId;
+//        String record = "NOT_FOUND";
+//        boolean Found = false;
+//
+//        while (!Found && (High >= Low))
+//        {
+//            Middle = (Low + High) / 2;
+//            record = getRecord(Din, Middle+1);
+//            MiddleId = record.substring(0,5);
+//
+//            int result = MiddleId.compareTo(id);
+//            if (result == 0)   // ids match
+//                Found = true;
+//            else if (result < 0)
+//                Low = Middle + 1;
+//            else
+//                High = Middle - 1;
+//        }
+//        if (Found)
+//            return record;
+//        else
+//            return "NOT_FOUND";
+//    }
+//}
 
 
 
